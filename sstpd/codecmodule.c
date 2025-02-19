f7ddd1e9d9a29589d2505f4422edb9fa6c5b2aba
kehav <kehav@icloud.com>
2025-02-19 19:49:26 +0300

    Fix strings buffers for Python 3.12 and older

--- sstpd/codecmodule.c
+++ sstpd/codecmodule.c
@@ -1,3 +1,4 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <stdbool.h>
 
@@ -148,6 +149,7 @@ PppDecoder_unescape(PppDecoder *self, PyObject *args)
                         self->frame_buf, self->frame_buf_pos - 2);
                 if (PyList_Append(frames, frame) == -1) {
                     Py_DECREF(frame);
+                    if (frame) Py_DECREF(frame);
                     self->frame_buf_pos = 0;
                     PyBuffer_Release(&buf_in);
                     return NULL;
